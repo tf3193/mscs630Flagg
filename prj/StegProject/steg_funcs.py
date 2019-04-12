e7354b567e7cad20a16841e1bdbc836bf16690c9
taflagg <tf3193@gmail.com>
2019-04-12 14:00:43 -0400

    Prelinimary work of redefining steg_funcs and web_funcs as classes. This breaks main

--- prj/StegProject/steg_funcs.py
+++ prj/StegProject/steg_funcs.py
@@ -2,84 +2,88 @@ import subprocess
 import os, signal, time
 
 
-def call_process(command):
-	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	out, err = process.communicate()
-	process.wait()
-	return out
+class stegHide:
+
+	def call_process(command):
+		process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		out, err = process.communicate()
+		process.wait()
+		return out
+
+	def steg_info(filenames):
+		sizes = []
+		for s in filenames:
+			process = subprocess.Popen(["steghide\steghide.exe", "info", "pictures/" + s], stderr=subprocess.STDOUT,
+									   stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+			out, err = process.communicate(b'n')
+			infoarray = str(out).split()
+			# This whole if/else is terrible and will be changed up.
+			if infoarray[19] == "KB":
+				# needs major testing
+				sizes.append(int(float(infoarray[18]) * 1024) - 100)
+			else:
+				sizes.append(int(float(infoarray[18])) - 100)
+		return sizes
+
+	def steg_version():
+		return call_process(["steghide\steghide.exe", "--version"])
 
 
-def steg_info(filenames):
-	sizes = []
-	for s in filenames:
+	def md5_pass(file):
+		return str.split(call_process(["md5sum", file]))[0]
 
-		process = subprocess.Popen(["steghide\steghide.exe", "info", "pictures/" + s], stderr=subprocess.STDOUT,
-		                           stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-		out, err = process.communicate(b'n')
-		infoarray = str(out).split()
-		#This whole if/else is terrible and will be changed up.
-		if infoarray[5] == "KB":
-			#needs major testing
-			sizes.append(int(float(infoarray[18]) * 1024) - 100)
-		else:
-			sizes.append(int(float(infoarray[18])) - 100)
-	return sizes
+	def steg_hide(files_to_hide, files_to_use, password):
+		print(files_to_hide)
+		print(files_to_use)
+		for h, u in zip(files_to_hide, files_to_use):
+			print(
+				call_process(["steghide\steghide.exe", "embed", "-ef", str(h), "-cf", "pictures/" + u, "-p", password]))
+			os.remove(str(h))
 
+	def steg_read(files_to_read, password):
+		"""
 
+		:param files_to_read:
+		:param password:
+		:return:
+		"""
+		filesarray = []
+		for s in files_to_read:
+			call_process(["steghide\steghide.exe", "extract", "-xf", "temp", "-sf", s, "-p", password, "-f"])
+			extracted_file = open("temp", "r")
+			filesarray.append(extracted_file.read())
+			extracted_file.close()
+			os.remove("temp")
+			os.remove(s)
+		return filesarray
 
-def steg_version():
-	return call_process(["steghide\steghide.exe", "--version"])
+	# reads images into an array from the image file
+	def read_images():
+		return os.listdir("pictures/")
 
+	# joins the file parts back together- not really needed.
+	def join_files(file_parts):
+		outfile = open("DECRYPTED", "w")
+		for i in file_parts:
+			outfile.write(i)
+	# outfile.write(i.read())
 
-#untested
-def md5_pass(file):
-	return str.split(call_process(["md5sum", file]))[0]
+	# returns an array of the file names written
+	def split_file(file_to_read, sizearray):
+		readbytes = 0
+		infile = open(file_to_read)
+		files_written = []
 
-
-def steg_hide(files_to_hide, files_to_use, password):
-	print(files_to_hide)
-	print(files_to_use)
-	for h, u in zip(files_to_hide, files_to_use):
-		print(call_process(["steghide\steghide.exe", "embed", "-ef", str(h), "-cf", "pictures/" + u, "-p", password]))
-		os.remove(str(h))
-
-
-def steg_read(files_to_read, password):
-	filesarray = []
-	for s in files_to_read:
-		call_process(["steghide\steghide.exe", "extract", "-xf", "temp", "-sf", s, "-p", password, "-f"])
-		extracted_file = open("temp", "r")
-		filesarray.append(extracted_file.read())
-		extracted_file.close()
-		os.remove("temp")
-		os.remove(s)
-	return filesarray
-
-
-# reads images into an array from the image file
-def read_images():
-	return os.listdir("pictures/")
-
-
-# joins the file parts back together- not really needed.
-def join_files(file_parts):
-	outfile = open("DECRYPTED", "w")
-	for i in file_parts:
-		outfile.write(i)
-		#outfile.write(i.read())
+		for i in sizearray:
+			# we're naming the file the read bytes
+			outfile = open(str(readbytes), "w+")
+			outfile.write(str(bytearray(infile.read(i), 'utf-8')))
+			files_written.append(readbytes)
+			readbytes += i
+		return files_written
 
 
-# returns an array of the file names written
-def split_file(file_to_read, sizearray):
-	readbytes = 0
-	infile = open(file_to_read)
-	files_written = []
 
-	for i in sizearray:
-		# we're naming the file the read bytes
-		outfile = open(str(readbytes), "w+")
-		outfile.write(str(bytearray(infile.read(i), 'utf-8')))
-		files_written.append(readbytes)
-		readbytes += i
-	return files_written
+
+
 
--- prj/StegProject/web_funcs.py
+++ prj/StegProject/web_funcs.py
@@ -10,40 +10,40 @@ from html.parser import HTMLParser
 global boo
 boo = []
 
+class webHostAndScraper:
 
-def start_http():
-	PORT = 8000
+	def start_http():
+		PORT = 8000
 
-	Handler = http.server.SimpleHTTPRequestHandler
+		Handler = http.server.SimpleHTTPRequestHandler
 
-	httpd = socketserver.TCPServer(("", PORT), Handler)
+		httpd = socketserver.TCPServer(("", PORT), Handler)
 
-	print("Serving at port", PORT)
-	httpd.serve_forever()
-
-
-def generate_index(string_array):
-	os.remove("index.html")
-	myfile = open("index.html", "a")
-	for s in string_array:
-		myfile.write("<IMG SRC=pictures/" + s + ">" + "\n")
+		print("Serving at port", PORT)
+		httpd.serve_forever()
 
 
+	def generate_index(string_array):
+		os.remove("index.html")
+		myfile = open("index.html", "a")
+		for s in string_array:
+			myfile.write("<IMG SRC=pictures/" + s + ">" + "\n")
 
-class MyHTMLParser(HTMLParser):
-	def handle_starttag(self, tag, attrs):
-		for attr in attrs:
-			boo.append(attr[1].split("/")[1])
+
+	class MyHTMLParser(HTMLParser):
+		def handle_starttag(self, tag, attrs):
+			for attr in attrs:
+				boo.append(attr[1].split("/")[1])
 
 
-def download_imgs(ip_addr):
-	urlobj = urllib.request.urlopen("http://" + ip_addr + ":8000")
-	web_pg = str(urlobj.read())
-	parser = MyHTMLParser()
-	parser.feed(web_pg)
-	imgsarray = []
-	# loops over the gathered image names and downloads them
-	for s in boo:
-		urllib.request.urlretrieve("http://" + ip_addr + ":8000/pictures/" + s, s)
-		imgsarray.append(s)
-	return imgsarray
+	def download_imgs(ip_addr):
+		urlobj = urllib.request.urlopen("http://" + ip_addr + ":8000")
+		web_pg = str(urlobj.read())
+		parser = MyHTMLParser()
+		parser.feed(web_pg)
+		imgsarray = []
+		# loops over the gathered image names and downloads them
+		for s in boo:
+			urllib.request.urlretrieve("http://" + ip_addr + ":8000/pictures/" + s, s)
+			imgsarray.append(s)
+		return imgsarray
