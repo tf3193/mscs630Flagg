e97f65720a5d603950d447ef2f63f6696160d087
Troy Flagg <tf3193@gmail.com>
2019-04-11 23:04:50 -0400

    this is terrible and needs major cleaning. This was hacked together quickly without much thought. Will clean up. Also probably breaks on anything but windows.

---
+++ prj/StegProject/README.md
@@ -0,0 +1,27 @@
+Security Algorithms Steganography Project
+====================
+
+
+#HOW TO RUN:
+create a "pictures" directory under src. This is where all pictures you use to hide your text file in will be stored.
+
+run `python main.py` and choose your options.
+
+####Option 1
+- After option 1 will ask you for a file, for example there bob.txt provided. 
+- Then a simple web server will spin up hosting your images with in this case holds bob.txt's data
+
+####Option 2
+- Provide the ip address(in this case it is localhost)
+- It will then scrape the web page for images and then extract your txt files contents out. 
+
+####Option 3
+- Does not work, do not use. 
+- In future it will give you a size that is available to hide your .txt file in. 
+
+
+#common errors:
+- if there are string conversion errors somewhere in steg_funcs.py, it probably means that 
+ a file that isn't a picture exists in your pictures folder.
+
+- many more....
---
+++ prj/StegProject/main.py
@@ -0,0 +1,65 @@
+import web_funcs as web
+import steg_funcs as steg
+import os
+
+
+#I do some bad stuff here of encoding and dumb decoding.
+def server():
+	print("Running as Server")
+	filename =input("What file do you want to serve?: ")
+	files = steg.read_images()
+	#array of image holding capacity
+	file_sizes = steg.steg_info(files)
+	#total room in pictures folder
+	total_room = sum(file_sizes)
+	infile = open(filename)
+	#gets the file size
+	infile.seek(0,2) # move the cursor to the end of the file
+	size = infile.tell()
+	print("File is "+ str(size) + " bytes, and total room is " + str(total_room))
+	if total_room < size:
+		print("Not enough space")
+		exit(1)
+	#now the file needs to be split and encoded into images
+	size_sum = 0
+	index = 0
+	while size_sum<size:
+		size_sum+=file_sizes[index]
+		index+=1
+	
+	splitarray = file_sizes[:index]
+	picstoencode = files[:index]
+	splitfiles = steg.split_file(filename, splitarray)
+	#the actual process of encoding the files into the images
+	steg.steg_hide(splitfiles, picstoencode,"password")
+	web.generate_index(picstoencode)
+	web.start_http()
+	exit(0)	
+	
+
+def client():
+	print("Running as client")
+	target_IP = input("Enter target IP: ")
+	imgarray = web.download_imgs(target_IP)
+	filesarray = steg.steg_read(imgarray, "password")
+	steg.join_files(filesarray)
+	exit(0)
+
+
+operation = input("Serve(1), Recieve(2), or Check Space(3) DONT PICK 3 it will blow things up: ")
+
+
+if operation == "1":
+	server()
+if operation == "2":
+	client()
+if operation == "3":
+	files = steg.read_images()
+	file_sizes = steg.steg_info(files)
+	#total room in pictures folder
+	totalbytes = sum(file_sizes)
+	print(str(totalbytes) + " bytes, or about " + str(round(float(totalbytes)/1024, 2)) + " KB")
+	
+else:
+	print("Incorrect Choice")
+	exit()
