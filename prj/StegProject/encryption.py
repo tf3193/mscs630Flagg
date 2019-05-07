91bbb6b42e55350f302be7663c47f9b024e62002
taflagg <tf3193@gmail.com>
2019-05-07 08:04:42 -0400

    Created a crypto class for encryption/decryption

---
+++ prj/StegProject/encryption.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES
+
+validKeyLength = [16, 24, 32]
+
+class CryptoMethods:
+    key = ""
+    cipher = ''
+    decipher = ''
+    def __init__(self, key):
+        if key.len() in validKeyLength:
+            self.key = key
+            self.cipher = AES.new(key, AES.MODE_ECB)
+            self.decipher = AES.new(key, AES.MODE_ECB)
+        else:
+            print("Key lengths must be 16, 24 or 32")
+            return 0
+
+    def encryptText(self, text):
+        return cipher.encrypt(text)
+
+    def decrypyText(self, text):
+        return decipher.decrypy(text)
--- prj/StegProject/web_funcs.py
+++ prj/StegProject/web_funcs.py
@@ -6,44 +6,48 @@ import os
 
 from html.parser import HTMLParser
 
-#Globals are fun.
+# Globals are fun.
 global boo
 boo = []
 
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            boo.append(attr[1].split("/")[1])
+
+
 class webHostAndScraper:
 
-	def start_http():
-		PORT = 8000
-
-		Handler = http.server.SimpleHTTPRequestHandler
-
-		httpd = socketserver.TCPServer(("", PORT), Handler)
-
-		print("Serving at port", PORT)
-		httpd.serve_forever()
 
 
-	def generate_index(string_array):
-		os.remove("index.html")
-		myfile = open("index.html", "a")
-		for s in string_array:
-			myfile.write("<IMG SRC=pictures/" + s + ">" + "\n")
+    def start_http():
+        PORT = 8000
+
+        Handler = http.server.SimpleHTTPRequestHandler
+
+        httpd = socketserver.TCPServer(("", PORT), Handler)
+
+        print("Serving at port", PORT)
+        httpd.serve_forever()
+
+    def generate_index(string_array):
+        os.remove("index.html")
+        myfile = open("index.html", "a")
+        for s in string_array:
+            myfile.write("<IMG SRC=pictures/" + s + ">" + "\n")
 
 
-	class MyHTMLParser(HTMLParser):
-		def handle_starttag(self, tag, attrs):
-			for attr in attrs:
-				boo.append(attr[1].split("/")[1])
 
-
-	def download_imgs(ip_addr):
-		urlobj = urllib.request.urlopen("http://" + ip_addr + ":8000")
-		web_pg = str(urlobj.read())
-		parser = MyHTMLParser()
-		parser.feed(web_pg)
-		imgsarray = []
-		# loops over the gathered image names and downloads them
-		for s in boo:
-			urllib.request.urlretrieve("http://" + ip_addr + ":8000/pictures/" + s, s)
-			imgsarray.append(s)
-		return imgsarray
+    def download_imgs(ip_addr):
+        urlobj = urllib.request.urlopen("http://" + ip_addr + ":8000")
+        web_pg = str(urlobj.read())
+        parser = MyHTMLParser()
+        parser.feed(web_pg)
+        print(parser.data)
+        imgsarray = []
+        # loops over the gathered image names and downloads them
+        for s in boo:
+            urllib.request.urlretrieve("http://" + ip_addr + ":8000/pictures/" + s, s)
+            imgsarray.append(s)
+        return imgsarray
